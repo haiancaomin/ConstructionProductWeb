a826ed90718556073a7256073d3907e183c9200f
haiancaomin <haiancaomin@126.com>
2019-09-19 12:02:09 +0800

    project/member

--- src/page/Teamwork/children/member.vue
+++ src/page/Teamwork/children/member.vue
@@ -13,16 +13,15 @@
           style="min-height: 10vw;"
         >
           <el-table :data="tableData" border style="width: 100%">
-            <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
-            <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-            <el-table-column prop="province" label="省份" width="120"></el-table-column>
-            <el-table-column prop="city" label="市区" width="120"></el-table-column>
+            <el-table-column fixed prop="name" label="姓名" width="120"></el-table-column>
+            <el-table-column prop="phone" label="联系方式" width="130"></el-table-column>
+            <el-table-column prop="company" label="所属公司" width="310"></el-table-column>
+            <el-table-column prop="project" label="所属项目" width="310"></el-table-column>
             <el-table-column prop="address" label="地址" width="300"></el-table-column>
-            <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
             <el-table-column fixed="right" label="操作" width="100">
               <template slot-scope="scope">
-                <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-                <el-button type="text" size="small">编辑</el-button>
+                <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+                <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -103,68 +102,11 @@ export default {
       total: 0,
       tableData: [
         {
-          date: "2016-05-03",
           name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
+          phone: "15195910513",
+          company: "智聚装配式绿色建筑创新中心南通有限公司",
+          project: "抗震支架M-3501 华新建工集团",
+          address: "上海市普陀区金沙江路 1518 弄"
         }
       ]
     };
